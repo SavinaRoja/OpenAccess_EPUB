7a54b3e8f144767f8a823e7838b3e098c2b865ee
Pablo Barton <pablo.barton@gmail.com>
2011-08-22 17:57:32 -0400

    Implementing article.py, refactored and commented main.py slightly to reflect new process

---
+++ article.py
@@ -0,0 +1,31 @@
+
+class Article(object):
+    '''A journal article; the top-level element (document element) of the 
+    Journal Publishing DTD, which contains all the metadata and content for 
+    the article
+    
+    http://dtd.nlm.nih.gov/publishing/tag-library/2.0/index.html
+    '''
+    
+    def __init__(self, doc):
+        self.root_tag = doc.documentElement
+        
+        attr_strings = [u'article-type', u'dtd-version', u'xml:lang', u'xmlns:mml', u'xmlns:xlink']
+        
+        attributes = {}
+        
+        if self.root_tag.attributes.length != 5:
+            raise ValueError('The <article> tag has an inappropriate number of attributes')
+        
+        for attr in attr_strings:
+            value = self.root_tag.getAttribute(attr)
+            attributes[attr] = value
+            
+        if attributes[u'xmlns:mml'] != u'http://www.w3.org/1998/Math/MathML':
+            raise ValueError('The MathML attribute value may not be changed from \'http://www.w3.org/1998/Math/MathML\'')
+        
+        if attributes[u'xmlns:xlink'] != u'http://www.w3.org/1999/xlink':
+            raise ValueError('The XLink Namespace Declaration attribute value may not be changed from \'http://www.w3.org/1999/xlink\'')
+        
+        if attributes[u'dtd-version'] != u'2.0':
+            raise ValueError('Version 2.0 of the Journal Publishing DTD must be used')
--- main.py
+++ main.py
@@ -6,14 +6,15 @@ import os.path
 import xml.dom.minidom as minidom
 
 import gofetch, metadata, bibliography, output, tocncx, parsebody
-from utils import OUT_DIR
+from article import Article
 
 def main():
     '''main script'''
     
-    parser = argparse.ArgumentParser(description = 'OpenAccess_EPUP Parser')
+    parser = argparse.ArgumentParser(description = 'OpenAccess_EPUB Parser')
     parser.add_argument('-q', '--quiet', action = 'store_true', default = False)
     parser.add_argument('-i', '--input', action = 'store', default = 'test_data/article.xml')
+    parser.add_argument('-o','--output', action = 'store', default = 'test_output/')
     
     args = parser.parse_args()
     
@@ -24,36 +25,37 @@ def main():
     dobiblio = False
     dotoc = False
     
-    doc = minidom.parse(args.input)
+    document = Article(minidom.parse(args.input))
+    
     
     # The <article> may have 4 parts defined parts
-    front = doc.getElementsByTagName('front')[0]
-    body = doc.getElementsByTagName('body')[0]
-    back = doc.getElementsByTagName('back')[0]
-    responses = doc.getElementsByTagName('response')
+    #front = doc.getElementsByTagName('front')[0]
+    #body = doc.getElementsByTagName('body')[0]
+    #back = doc.getElementsByTagName('back')[0]
+    #responses = doc.getElementsByTagName('response')
     
     #The FrontMatter object handles the material in the <front> of the article
     #ArticleMeta and JournalMeta objects are instantiated and held as 
     #attributes of FrontMatter
-    frontmatter = metadata.FrontMatter(front)
-    bodymatter = parsebody.BodyMatter(body, doc)
+    #frontmatter = metadata.FrontMatter(front)
+    #bodymatter = parsebody.BodyMatter(body, doc)
     
-    filename = os.path.join(OUT_DIR, output.getTitle_str(frontmatter))
+    #filename = os.path.join(OUT_DIR, output.getTitle_str(frontmatter))
     
-    output.generateFrontpage(frontmatter)
+    #output.generateFrontpage(frontmatter)
     
-    if dobiblio:
-        biblio = bibliography.Biblio(doc)
-        biblio.output()
+    #if dobiblio:
+    #    biblio = bibliography.Biblio(doc)
+    #    biblio.output()
+    #
+    #if dooutput:
+    #    output.generateHierarchy(filename)
+    #    output.epubZip(filename)
+    #if dofetch:
+    #    gofetch.getimages(frontmatter, filename)
     
-    if dooutput:
-        output.generateHierarchy(filename)
-        output.epubZip(filename)
-    if dofetch:
-        gofetch.getimages(frontmatter, filename)
-    
-    if dotoc:
-        tocncx.generateTOC(frontmatter)
+    #if dotoc:
+    #    tocncx.generateTOC(frontmatter)
         
 if __name__ == '__main__':
     main()
