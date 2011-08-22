9fc5bfaacec75fe68274fecd25f483e67a154929
Pablo Barton <pablo.barton@gmail.com>
2011-08-22 14:59:51 -0400

    modified main.py to be more pythonic, began utiliizing argparse

--- main.py
+++ main.py
@@ -1,43 +1,62 @@
-"""main script"""
-import gofetch, metadata, bibliography, output, tocncx, parsebody
-from utils import OUT_DIR
+#! /usr/bin/python
+
+import argparse
+import sys
 import os.path
 
-dofetch = False
-dooutput = False
-dobiblio = False
-dotoc = False
+import gofetch, metadata, bibliography, output, tocncx, parsebody
+from utils import OUT_DIR
 
-import xml.dom.minidom as minidom
 
-ARTICLE = 'test_data/article.xml'
-doc = minidom.parse(ARTICLE)
 
-# The <article> may have 4 parts defined parts
-front = doc.getElementsByTagName('front')[0]
-body = doc.getElementsByTagName('body')[0]
-back = doc.getElementsByTagName('back')[0]
-responses = doc.getElementsByTagName('response')
+def main():
+    '''main script'''
+    
+    parser = argparse.ArgumentParser(description = 'OpenAccess_EPUP Parser')
+    parser.add_argument('-q', '--quiet', action = 'store_true', default = False)
+    
+    args = parser.parse_args()
+    
+    isQuiet = args.quiet
+    
+    dofetch = False
+    dooutput = False
+    dobiblio = False
+    dotoc = False
 
-#The FrontMatter object handles the material in the <front> of the article
-#ArticleMeta and JournalMeta objects are instantiated and held as 
-#attributes of FrontMatter
-frontmatter = metadata.FrontMatter(front)
-bodymatter = parsebody.BodyMatter(body, doc)
+    import xml.dom.minidom as minidom
 
-filename = os.path.join(OUT_DIR, output.getTitle_str(frontmatter))
-
-output.generateFrontpage(frontmatter)
-
-if dobiblio:
-    biblio = bibliography.Biblio(doc)
-    biblio.output()
-
-if dooutput:
+    ARTICLE = 'test_data/article.xml'
+    doc = minidom.parse(ARTICLE)
+    
+    # The <article> may have 4 parts defined parts
+    front = doc.getElementsByTagName('front')[0]
+    body = doc.getElementsByTagName('body')[0]
+    back = doc.getElementsByTagName('back')[0]
+    responses = doc.getElementsByTagName('response')
+    
+    #The FrontMatter object handles the material in the <front> of the article
+    #ArticleMeta and JournalMeta objects are instantiated and held as 
+    #attributes of FrontMatter
+    frontmatter = metadata.FrontMatter(front)
+    bodymatter = parsebody.BodyMatter(body, doc)
+    
+    filename = os.path.join(OUT_DIR, output.getTitle_str(frontmatter))
+    
+    output.generateFrontpage(frontmatter)
+    
+    if dobiblio:
+        biblio = bibliography.Biblio(doc)
+        biblio.output()
+    
+    if dooutput:
     output.generateHierarchy(filename)
     output.epubZip(filename)
     if dofetch:
         gofetch.getimages(frontmatter, filename)
     
-if dotoc:
-    tocncx.generateTOC(frontmatter)
+    if dotoc:
+        tocncx.generateTOC(frontmatter)
+        
+if __name__ == '__main__':
+    main()
