9772b673ef3044ada5f95db5f6573cd5023fe75c
Pablo Barton <pablo.barton@gmail.com>
2011-08-30 17:54:11 -0400

    Created fetchImages function for Article to retrieve online data, deprecating gofetch.py

--- article.py
+++ article.py
@@ -56,6 +56,73 @@ class Article(object):
         
         return titlestring
         
+    def fetchImages(self, publisher = 'PLoS', dirname = 'test_output'): #Default to PLoS for now
+        '''Fetch the images associated with the article.'''
+        
+        import urllib2, logging, os.path
+        
+        for (_data, _id) in self.front.article_meta.identifiers:
+            if _id == 'doi':
+                doidata = _data
+        
+        if publisher == 'PLoS':
+            
+            PLOSSTRING = 'article/fetchObject.action?uri=info%3Adoi%2F'
+            
+            print(doidata)
+            #split the doidata into useful fragments
+            slashsplit = doidata.split('/')
+            journaldoi = slashsplit[0]
+            dotsplit = slashsplit[1].split('.')
+            journalid = dotsplit[1]
+            articledoi = dotsplit[2]
+            
+            if journalid == 'pgen':
+                journalurl = 'http://www.plosgenetics.org/'
+            elif journalid == 'pcbi':
+                journalurl = 'http://www.ploscompbiol.org/'
+            elif journalid == 'ppat':
+                journalurl = 'http://www.plospathogens.org/'
+            elif journalid == 'pntd':
+                journalurl = 'http://www.plosntds.org/'
+            elif journalid == 'pmed':
+                journalurl = 'http://www.plosmedicine.org/'
+            elif journalid == 'pbio':
+                journalurl = 'http://www.plosbiology.org/'
+            elif journalid == 'pone':
+                journalurl = 'http://www.plosone.org/'
+            
+            imagetypes = ['g', 't', 'e']
+            
+            for itype in imagetypes:
+                if itype == 'g':
+                    subdirect = 'figures'
+                elif itype == 't':
+                    subdirect = 'tables'
+                elif itype == 'e':
+                    subdirect = 'equations'
+                    
+                for refnum in range(1,1000):
+                    addr_str = '{0}{1}{2}%2Fjournal.{3}.{4}.{5}{6}&representation=PNG_L'
+                    address = addr_str.format(journalurl, PLOSSTRING, journaldoi,
+                                              journalid, articledoi, itype,
+                                              str(refnum).zfill(3))
+                    try:
+                        print(address)
+                        if itype == 'e':
+                            address = address[0:-2]
+                        image = urllib2.urlopen(address)
+                        filename = '{0}{1}.png'.format(itype, str(refnum).zfill(3))
+                        image_file = os.path.join(dirname, 'OPS', 'images',
+                                                  subdirect, filename)
+                        with open(image_file, 'wb') as outimage:
+                            outimage.write(image.read())
+                    
+                    except urllib2.HTTPError:
+                        logging.debug('reached the end of that type')
+                        break
+                    refnum += 1
+            
     def output_epub(self, directory):
         import output
         
