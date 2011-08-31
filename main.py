33c7c0b9b36a02fb3f6ba9db86157df033248aca
Pablo Barton <pablo.barton@gmail.com>
2011-08-31 18:05:32 -0400

    Cleaned up main.py, removing old comments and commenting out fetchImages for streamlined testing

--- main.py
+++ main.py
@@ -24,37 +24,8 @@ def main():
     
     if args.output:
         document.output_epub(args.output)
-        document.fetchImages()
+        #document.fetchImages()
         output.epubZip(args.output, document.titlestring())
     
-    # The <article> may have 4 parts defined
-    #front = doc.getElementsByTagName('front')[0]
-    #body = doc.getElementsByTagName('body')[0]
-    #back = doc.getElementsByTagName('back')[0]
-    #responses = doc.getElementsByTagName('response')
-    
-    #The FrontMatter object handles the material in the <front> of the article
-    #ArticleMeta and JournalMeta objects are instantiated and held as 
-    #attributes of FrontMatter
-    #frontmatter = metadata.FrontMatter(front)
-    #bodymatter = parsebody.BodyMatter(body, doc)
-    
-    #filename = os.path.join(OUT_DIR, output.getTitle_str(frontmatter))
-    
-    #output.generateFrontpage(frontmatter)
-    
-    #if dobiblio:
-    #    biblio = bibliography.Biblio(doc)
-    #    biblio.output()
-    #
-    #if dooutput:
-    #    output.generateHierarchy(filename)
-    #    output.epubZip(filename)
-    #if dofetch:
-    #    gofetch.getimages(frontmatter, filename)
-    
-    #if dotoc:
-    #    tocncx.generateTOC(frontmatter)
-        
 if __name__ == '__main__':
     main()
