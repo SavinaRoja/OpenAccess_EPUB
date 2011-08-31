9d97831517dbaabab6bfc257771b2c4b3d0a6e36
Pablo Barton <pablo.barton@gmail.com>
2011-08-31 18:04:52 -0400

    Updated to make the methods safe for different newline parsing

--- epub_date.py
+++ epub_date.py
@@ -23,28 +23,24 @@ class DateInfo(object):
         datenode -- the XML Element containing the date info
         returns a 3-tuple: (year, month, day)
         """
-        # set some defaults
-        year = 0
-        month = 1
-        day = 1
         
         if datenode.tagName == 'date':
             self.type = datenode.getAttribute('date-type')
         elif datenode.tagName == 'pub-date':
             self.type = datenode.getAttribute('pub-type')
         
-        child = datenode.firstChild
-        if child.tagName == 'season':
-            self.season = child.firstChild.data
-        elif child.tagName == 'day':
-            day = int(child.firstChild.data)
-            if child.nextSibling:
-                month = int(child.nextSibling.firstChild.data)
-        elif child.tagName == 'month':
-            month = int(child.firstChild.data)
+        try:
+            self.season = datenode.getElementsByTagName('season')[0].firstChild.data
+        except IndexError:
+            try:
+                day = int(datenode.getElementsByTagName('day')[0].firstChild.data)
+            except IndexError:
+                day = 1
+            try:
+                month = int(datenode.getElementsByTagName('month')[0].firstChild.data)
+            except IndexError:
+                month = 1
         
-        # year is always the last element
-        child = datenode.lastChild
-        year = int(child.firstChild.data)
+        year = int(datenode.getElementsByTagName('year')[0].firstChild.data)
         
         return (year, month, day)
