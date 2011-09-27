9d22219ee2a0f9a98042007e4ed95c4d5b8722b7
Pablo Barton <pablo.barton@gmail.com>
2011-09-27 15:00:43 -0400

    Further development of tocncx.py

--- tocncx.py
+++ tocncx.py
@@ -33,10 +33,8 @@ def generateTOC(fm, features):
                 navpoint.appendChild(navnode)
                 navnode.setAttribute('playOrder',child.getAttribute('playOrder'))
                 navnode.setAttribute('id', child.getAttribute('id'))
-                titletext = child.getElementsByTagName('title')[0]
-                for item in titletext.childNodes:
-                    if item.nodeType == item.TEXT_NODE and not item.data == u'\n':
-                        titletext = item.data
+                titlenode = child.getElementsByTagName('title')[0]
+                titletext = utils.serializeText(titlenode, stringlist = [])
                 navlabel = doc.createElement('navLabel')
                 navnode.appendChild(navlabel)
                 navlabel.appendChild(makeText(titletext))
@@ -46,19 +44,55 @@ def generateTOC(fm, features):
                 navmapper(child, navnode)
     
     def listFigures(featurenode, navpoint):
-        for each in featurenode.getElementsByTagName('fig'):
+        for child in featurenode.getElementsByTagName('fig'):
             navtarget = doc.createElement('navTarget')
             navtarget.setAttribute('class', 'figure')
+            navtarget.setAttribute('playOrder',child.getAttribute('playOrder'))
+            navtarget.setAttribute('id', child.getAttribute('id'))
             navlabel = doc.createElement('navLabel')
+            titlenode = child.getElementsByTagName('title')[0]
+            titletext = utils.serializeText(titlenode, stringlist = [])
+            navlabel.appendChild(makeText(titletext))
             navtarget.appendChild(navlabel)
-            navlabel.appendChild(makeText(''))
             content = doc.createElement('content')
             content.setAttribute('src', '')
+            navtarget.appendChild(content)
+            navpoint.appendChild(navtarget)
+            
+    def listTables(featurenode, navpoint):
+        for child in featurenode.getElementsByTagName('table'):
+            navtarget = doc.createElement('navTarget')
+            navtarget.setAttribute('class', 'table')
+            navlabel = doc.createElement('navLabel')
+            titlenode = child.getElementsByTagName('title')[0]
+            titletext = utils.serializeText(titlenode, stringlist = [])
+            navlabel.appendChild(makeText(titletext))
+            navtarget.appendChild(navlabel)
+            content = doc.createElement('content')
+            content.setAttribute('src', '')
+            navtarget.appendChild(content)
+            navpoint.appendChild(navtarget)
+            
+    def listEquations(featurenode, navpoint):
+        children = featurenode.getElementsByTagName('inline-formula')
+        children += featurenode.getElementsByTagName('disp-formula')
+        for child in children:
+            navtarget = doc.createElement('navTarget')
+            navtarget.setAttribute('class', 'equation')
+            navlabel = doc.createElement('navLabel')
+            titlenode = child.getElementsByTagName('title')[0]
+            titletext = utils.serializeText(titlenode, stringlist = [])
+            navlabel.appendChild(makeText(titletext))
+            navtarget.appendChild(navlabel)
+            content = doc.createElement('content')
+            content.setAttribute('src', '')
+            navtarget.appendChild(content)
+            navpoint.appendChild(navtarget)
     
     ncx = doc.lastChild #IGNORE:E1101
     ncx.setAttribute('version', '2005-1')
     ncx.setAttribute('xml:lang', 'en-US')
-    ncx.setAttribute('xmlns', 'http://www.daisy.org/z3986/2005/Z3986-2005.html')
+    ncx.setAttribute('xmlns', 'http://www.daisy.org/z3986/2005/ncx/')
     
     ncx_elements = ['head', 'docTitle', 'docAuthor', 'navMap']
     for element in ncx_elements:
@@ -73,6 +107,8 @@ def generateTOC(fm, features):
         lof.appendChild(navlabel)
         navlabel.appendChild(makeText('List of Figures'))
         ncx.appendChild(lof)
+        #Create the navList element for the list of figures
+        listFigures(features, lof)
     
     if features.getElementsByTagName('table'):
         lot = doc.createElement('navList')
@@ -82,6 +118,8 @@ def generateTOC(fm, features):
         lot.appendChild(navlabel)
         navlabel.appendChild(makeText('List of Tables'))
         ncx.appendChild(lot)
+        #Create the navList element for the list of tables
+        listTables(features, lot)
         
     if features.getElementsByTagName('inline-formula') or features.getElementsByTagName('disp-formula'):
         loe = doc.createElement('navList')
@@ -91,6 +129,9 @@ def generateTOC(fm, features):
         loe.appendChild(navlabel)
         navlabel.appendChild(makeText('List of Equations'))
         ncx.appendChild(lot)
+        #Create the navList element for the list of equations
+        listEquations(features, loe)
+        
     
     statement = doc.createComment('''The following metadata items, except for dtb:generator,
             are required for all NCX documents, including those 
@@ -123,6 +164,11 @@ def generateTOC(fm, features):
     doctitle.appendChild(makeText(tocname))
     docauthor.appendChild(makeText('{0}{1}'.format('Primary author: ', 
                                                    fm.article_meta.art_auths[0].get_name())))
+    
+    #Create the navMap element
+    navlabel = doc.createElement('navLabel')
+    navmap.appendChild(navlabel)
+    navlabel.appendChild(makeText('Table of Contents'))
     navmapper(features, navmap)
     
     outdoc = open('{0}/OPS/toc.ncx'.format(utils.OUT_DIR),'w')
