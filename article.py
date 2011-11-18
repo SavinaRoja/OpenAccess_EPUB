0ca4e2b60dcb95f79dd404119c5088ce117efbb2
Paul <pablo.barton@gmail.com>
2011-11-18 15:18:14 -0500

    Moving Front Class to article.py

--- article.py
+++ article.py
@@ -1,4 +1,5 @@
 import utils
+import metadata
 import front, body
 import xml.dom.minidom as minidom
 
@@ -50,7 +51,7 @@ class Article(object):
         #    except:
         #        pass
         
-        self.front = front.Front(frontnode)
+        self.front = Front(frontnode)
         self.body = body.Body(self.bodynode)
         self.back = Back(self.backnode)
         
@@ -201,7 +202,27 @@ class Article(object):
                         self.playorder += 1
                         destnode.appendChild(clone)
                         self.featureParse(doc, child, clone)
-                        
+
+class Front(object):
+    
+    '''
+    The metadata for an article, such as the name and issue of the journal 
+    in which the article appears and the author(s) of the article
+    '''
+    
+    def __init__(self, node):
+        #Front may have no attributes
+        self.root_tag = node
+        
+        #It must have a <journal-meta>
+        journalmetanode = self.root_tag.getElementsByTagName('journal-meta')[0]
+        self.journal_meta = metadata.JournalMeta(journalmetanode)
+        
+        #It must have a <article-meta>
+        articlemetanode = self.root_tag.getElementsByTagName('article-meta')[0]
+        self.article_meta = metadata.ArticleMeta(articlemetanode)
+        
+
 class Back(object):
     
     def __init__(self, node):
