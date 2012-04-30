84a91ac1060980ffa969b0910406b869d48569b4
Paul Barton <pablo.barton@gmail.com>
2012-04-30 15:12:59 -0400

    Introducing jptscontrib file

---
+++ jptscontrib.py
@@ -0,0 +1,7 @@
+"""
+This module will provide useful classes and functions for representing
+article contributions as defined in the Journal Publishing Tag Set.
+
+Temporary Note: This module will functionally take the place of contributor.py
+and that file will be removed soon.
+"""
