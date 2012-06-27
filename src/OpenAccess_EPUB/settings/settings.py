a9915cb832b52e90564bb7b82e41e8de8829455e
Paul Barton <pablo.barton@gmail.com>
2012-06-27 09:18:13 -0400

    Added logging for the settings module, includes debugging log records for system locations

--- src/OpenAccess_EPUB/settings/settings.py
+++ src/OpenAccess_EPUB/settings/settings.py
@@ -1,5 +1,6 @@
 import os.path
 import sys
+import logging
 
 
 class Settings(object):
@@ -8,7 +9,7 @@ class Settings(object):
     """
     def __init__(self):
         #This acquires the local directory
-        local = os.getcwd()
+        self.local = os.getcwd()
 
         #This acquires the appropriate directory for storing static/global data
         #If the user encounters an error, or wants to customize the location
@@ -21,10 +22,8 @@ class Settings(object):
         #This sets the cache_location
         self.cache_img = os.path.join(self.cache_loc, 'img_cache')
 
-        #Logging is a good idea, best to leave True
-        self.logging = True
         #This sets the location for storing log files
-        self.local_log = os.path.join(local, 'logs')
+        self.local_log = os.path.join(self.local, 'logs')
         self.save_log = True
         self.cache_log = os.path.join(self.cache_loc, 'logs')
 
@@ -37,8 +36,8 @@ class Settings(object):
         #This sets a default output location relative to the directory in which
         #the code is run.
         self.save_output = False
-        self.local_output = os.path.join(local, 'output')
-        #self.local_output = local
+        self.local_output = os.path.join(self.local, 'output')
+        #self.local_output = self.local
         self.cache_output = os.path.join(self.cache_loc, 'output')
 
         #This determines whether the program will erase the pre-zipped output
@@ -52,11 +51,14 @@ class Settings(object):
 
         #This determines the location of the base css file, which is copied to
         #the ePub's css directory
-        self.local_css = os.path.join(local, 'css')
+        self.local_css = os.path.join(self.local, 'css')
         self.cache_css = os.path.join(self.cache_loc, 'css')
 
         #Configure the location of epubcheck-*.jar.
         self.epubcheck = '../epubcheck/epubcheck-3.0b3.jar'
+
+        #Record settings in log
+        self.logSettings()
 
     def cacheLocation(self):
         """
@@ -72,3 +74,14 @@ class Settings(object):
         if os.name == 'nt':
             s = os.environ['APPDATA']
             return os.path.join(s, '.OpenAccess_EPUB')
+
+    def logSettings(self):
+        """
+        Make log statements for settings which might be useful for debugging.
+        """
+        log = logging.getLogger('Settings')
+        log.debug('Cache Location: {0}'.format(self.cache_loc))
+        log.debug('Local: {0}'.format(self.local))
+        log.debug('Local Output: {0}'.format(self.local_output))
+        log.debug('Local CSS: {0}'.format(self.local_css))
+        log.debug('Cleanup: {0}'.format(self.cleanup))
