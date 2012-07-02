95d23de7c25c3a8578f5713d3a55b105b07337f3
Paul Barton <pablo.barton@gmail.com>
2012-07-02 10:16:03 -0400

    Emplacing files for the new bibliography and Chigao Manual of Style modules

---
+++ src/OpenAccess_EPUB/biblio/cms/cms.py
@@ -0,0 +1,5 @@
+"""
+The Chicago Manual of Style specifies the textual formatting style of
+bibliographic citations. This module works with xml.dom to parse the xml
+metadata of a bibliographic reference into a correctly formatted string.
+"""
