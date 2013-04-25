e65336aea423b635de9e36388d05a1080cb473d3
SavinaRoja <pablo.barton@gmail.com>
2013-04-25 10:13:49 -0400

    Added a defaults flag, a formatter method with demonstration, and a few more configurables

--- src/openaccess_epub/quickstart.py
+++ src/openaccess_epub/quickstart.py
@@ -6,17 +6,20 @@ Used for configuring an installation of OpenAccess_EPUB
 """
 
 from openaccess_epub.utils import cache_location
+from openaccess_epub import __version__ as OAE_VERSION
 import os
 import sys
+import time
 
 LOCAL_DIR = os.getcwd()
 CACHE_LOCATION = cache_location()
 
-CONFIG_TEXT='''/
-# -*- coding: utf-8 -*-
+CONFIG_TEXT='''# -*- coding: utf-8 -*-
 #
 # OpenAccess_EPUB configuration file, created by oae-quickstart on
-# {now}. The script detected OpenAccess_EPUB v.{version} at that time.
+# {now}.
+#
+# The script detected OpenAccess_EPUB v.{oae-version} at that time.
 #
 # At this point in time, all possible values for configuration are represented
 # in this file. Suggested defaults exist for all values, but each may receive
@@ -32,34 +35,11 @@ import logging
 
 # oaepub needs to be able to reliably find this config file; it will always be
 # located in the directory returned by openaccess_epub.utils.cache_location().
-# This directory is:  (path string)
+# This directory is:
 
 cache_location = '{cache-location}'
 
-# -- General Caching Configuration --------------------------------------------
 
-# Image caching is helpful mostly for developers without local access to images
-# which go into ePubs, this allows one to avoid re-downloading images during
-# testing. Use image caching? (boolean)
-
-image_caching = {image-caching}
-
-# Where should the image cache be located? Use an absolute path. (path string)
-
-image_cache = '{image-cache}'
-
-# Log caching is helpful mostly for reporting errors that may be found some
-# time after content creation and the original log may be missing. This option
-# may be uncommon, and it defaults to False (boolean).
-
-log_caching = {log-caching}
-
-# Where should the log cache be located? Use an absolute path. (path string)
-
-log_cache = '{log-cache}'
-
-# Unless OpenAccess_EPUB is able to locate a local CSS file for inclusion in
-# the ePub output, it will attempt to 
 '''
 
 class ValidationError(Exception):
@@ -92,21 +72,40 @@ def user_prompt(config_dict, key, text, default=None, validator=nonempty):
         break
     config_dict[key] = user_inp
 
+def config_formatter(config_string, config_dict):
+    """
+    Places the config dictionary values into the appropriate keynamed areas in
+    the config text.
+    """
+    return(config_string.format(**config_dict))
+
 def inner_main(args):
     """
     The inner control loops for user interaction during quickstart
     configuration.
     """
 
-    default_config={'input-relative': 'images',
+    default_config={'now': time.asctime(),
+                    'oae-version': OAE_VERSION,
+                    'cache-location': CACHE_LOCATION,
+                    'input-relative': 'images',
                     'use-input-relative': 'y',
                     'execution-relative': 'images',
                     'use-execution-relative': 'n',
                     'image-cache': os.path.join(CACHE_LOCATION, 'img_cache'),
                     'use-image-cache': 'n',
-                    'use-image-fetching': 'y'}
+                    'use-image-fetching': 'y',
+                    'default-output': '.{0}'.format(os.sep)}
 
-    config_dict = {}
+    if args[-1] == '-d':  # Use all default options flag
+        config = config_formatter(CONFIG_TEXT, default_config)
+        with open(os.path.join(CACHE_LOCATION, 'config.py'), 'wb') as conf_out:
+            conf_out.write(bytes(config, 'UTF-8'))
+        return
+
+    config_dict = {'now': time.asctime(),
+                   'oae-version': OAE_VERSION,
+                   'cache-location': CACHE_LOCATION}
 
     print('\nWelcome to the quickstart configuration for OpenAccess_EPUB')
     print('''
@@ -190,8 +189,19 @@ is not supported for all publishers and not 100% guaranteed to succeed.''')
                 validator=boolean)
     #Output configuration
     print('''
- -- Configure Image Behavior --
-''')
+ -- Configure Output Behavior --
+
+OpenAccess_EPUB produces ePub and log files as output. The following options
+will determine what is done with these.
+
+Where should OpenAccess_EPUB place the output ePub and log files? If you supply
+a relative path, the output path will be relative to the input; if you supply
+an absolute path, the output will always be placed there. The default behavior
+is to place them in the same directory as the input.''')
+    user_prompt(config_dict, 'default-output', 'Output path?:',
+                default=default_config['default-output'],
+                validator=nonempty)
+    
 
 
     for key in config_dict.keys():
