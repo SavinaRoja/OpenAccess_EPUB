10e71d7a8ba6ea6cc44262d28cc9b3f6dd8eac6e
SavinaRoja <pablo.barton@gmail.com>
2013-05-13 11:53:43 -0400

    adding long description

--- setup.py
+++ setup.py
@@ -4,9 +4,15 @@ This is the setup file to install openaccess_epub.
 
 from distutils.core import setup
 
+def long_description():
+    with open('README.md', 'r') as readme:
+        readme_text = readme.read()
+    return(readme_text)
+
 setup(name='openaccess_epub',
       version='0.3.2',
       description='Converts OpenAccess Journal articles to ePub',
+      long_description=long_description(),
       author='Paul Barton',
       author_email='pablo.barton@gmail.com',
       url='https://github.com/SavinaRoja/openaccess_epub',
