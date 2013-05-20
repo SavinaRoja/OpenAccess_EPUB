fe33399ad223fd16c2fa1b0d4c6a780526c02a41
SavinaRoja <pablo.barton@gmail.com>
2013-05-20 10:36:20 -0400

    Adding some better comments for clarity in the CSS

--- src/openaccess_epub/utils/css.py
+++ src/openaccess_epub/utils/css.py
@@ -3,7 +3,10 @@
 For the creation of default CSS files.
 """
 
-DEFAULT_CSS='''div.abstract,
+DEFAULT_CSS='''/*A default CSS page, developed while working on PLoS*/
+
+/*Some divs are classed for special treatment*/
+div.abstract,
 div.editorsAbstract,
 div.summary,
 div.articleInfo
@@ -12,20 +15,31 @@ padding:0 10px;
 margin:20px 0 40px 0;
 border-left:5px solid #CCE3F6;
 }
+
+/*The ArticleInfo is a special segment of an article, it contains content that
+should be distinguishable from the main text.*/
 #ArticleInfo
 {
 border-top: 1px dashed;
 border-bottom: 1px dashed;
 }
+
+/**/
 span.footnote
 {
 display:block;
 }
+
+/*The disp-quote is for extended quotations, extracts, etc. they should be
+notably separate in display from the main text body.*/
 .disp-quote
 {
 display:block;
 margin-left:5%;
 }
+
+/*The common forms of images in articles are figures, formulas, and tables;
+Special style rules for these img elements go here.*/
 img.disp-formula
 {
 display:block;
@@ -42,21 +56,26 @@ display:block;
 margin-left:auto;
 margin-right:auto;
 }
+
 b.disp-formula-label
 {
 display:block;
 text-align:right;
 }
+
 .boxed-text
 {
 margin:5%;
 background-color:#EEEEEE;
 }
+
 p.verse-line
 {
 font-style:italic;
 line-height:50%;
 }
+
+
 p.def-item-term
 {
 font-weight:bold;
@@ -66,6 +85,9 @@ p.def-item-def
 font-style:italic;
 margin-left:5%;
 }
+
+/*These are for the handling of lists, to give them the appropriate list item
+indicators.*/
 ul.simple
 {
 list-style-type:none;
