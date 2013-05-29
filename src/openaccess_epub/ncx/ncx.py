be57e1675b494e2eb8c41e569241fd27045a117c
SavinaRoja <pablo.barton@gmail.com>
2013-05-29 16:40:14 -0400

    adding some content to the new ncx docstring

--- src/openaccess_epub/ncx/ncx.py
+++ src/openaccess_epub/ncx/ncx.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 """
+This module provides the basic tools necessary to create the NCX (Navigation
+Center eXtended) file for ePub. The specification for this file is a subsection
+of the OPF specification. See:
 
+http://www.idpf.org/epub/20/spec/OPF_2.0.1_draft.htm#Section2.4.1
+http://www.niso.org/workrooms/daisy/Z39-86-2005.html#NCX
+
+The NCX file is REQUIRED for valid ePub, but readers support it OPTIONALLY. Its
+job is to provide a more advanced Table of Contents-style navigation system for
+the ePub beyond what is done in the OPF file.
 """
 
 class NCX(object):
