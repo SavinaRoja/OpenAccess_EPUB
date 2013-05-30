ad0793df28620c9ab28bcf43701a94fa8e07df65
SavinaRoja <pablo.barton@gmail.com>
2013-05-30 13:52:30 -0400

    some framework for ncx

--- src/openaccess_epub/ncx/ncx.py
+++ src/openaccess_epub/ncx/ncx.py
@@ -40,29 +40,26 @@ class NCX(object):
     difference to Single Input unless one is using an unusual workflow (such as
     using the same NCX instance to generate .ncx files for different ePubs).
     """
-    def __init__(self):
+    def __init__(self, oae_version, location, location=os.getcwd(),
+                 collection_mode=False):
         """
         Initialization arguments:
+            oae_version - Version of OpenAccess_Epub; needed to specify in the
+              header what version of OAE generated the toc.ncx file
             location - Where this ePub is based
             collection_mode - To use Collection Mode, set to True
-            title - What the ePub will be titled*
 
         Collection Mode can be turned on or off after initialization using the
         use_collection_mode() and use_single_mode methods() respectively.
-
-        *An ePub's title is determined by the value of the <dc:title> element in
-        the content.opf file. In Single Mode, this title is determined
-        automatically from the article it receives in take_article(). If one
-        wishes to create a workflow for Single Mode that uses a different title
-        then use the OPF class method set_title() after passing an article to
-        take_article(). In Collection Mode, the title must be manually supplied
-        in some form or the ePub will have an empty string for a title that
-        ought to be corrected manually afterwards. Initializing the OPF
-        instance with the title argument, or calling set_title() at any time
+e
         before writing will give it a title.
+        """
         #Set internal variables to defaults
         self.reset_state()
-        """
+        #Pull in argument values
+        self.version = oae_version
+        self.location = location
+        self.collection_mode = collection_mode
 
     def init_NCX_document(self):
         """
@@ -73,35 +70,75 @@ class NCX(object):
         impl = xml.dom.minidom.getDOMImplementation()
         doctype = impl.createDocumentType('ncx', publicId, systemId)
         self.doc = impl.createDocument(None, 'ncx', doctype)
-        self.ncx = self.doc.lastChild
+        self.ncx = self.document.lastChild
         self.ncx.setAttribute('version', '2005-1')
         self.ncx.setAttribute('xml:lang', 'en-US')
         self.ncx.setAttribute('xmlns', 'http://www.daisy.org/z3986/2005/ncx/')
         #Create the sub elements to <ncx>
         ncx_subelements = ['head', 'docTitle', 'docAuthor', 'navMap']
         for element in ncx_subelements:
-            self.ncx.appendChild(self.doc.createElement(element))
+            self.ncx.appendChild(self.document.createElement(element))
         self.head, self.doctitle, self.docauthor, self.navmap = self.ncx.childNodes
         #Add a label with text 'Table of Contents' to navMap
         lbl = self.appendNewElement('navLabel', self.navmap)
         lbl.appendChild(self.make_text('Table of Contents'))
         #Create some optional subelements
         #These are not added to the document yet, as they may not be needed
-        self.list_of_figures = self.doc.createElement('navList')
-        self.list_of_figures.setAttribute('class', 'lof')
-        self.list_of_figures.setAttribute('id', 'lof')
-        self.list_of_tables = self.doc.createElement('navList')
-        self.list_of_tables.setAttribute('class', 'lot')
-        self.list_of_tables.setAttribute('id', 'lot')
+        #self.list_of_figures = self.document.createElement('navList')
+        #self.list_of_figures.setAttribute('class', 'lof')
+        #self.list_of_figures.setAttribute('id', 'lof')
+        #self.list_of_tables = self.document.createElement('navList')
+        #self.list_of_tables.setAttribute('class', 'lot')
+        #self.list_of_tables.setAttribute('id', 'lot')
         #The <head> element requires some basic content
-        self.head.appendChild(self.doc.createComment('''The following metadata
+        self.head.appendChild(self.document.createComment('''The following metadata
 items, except for dtb:generator, are required for all NCX documents, including
 those conforming to the relaxed constraints of OPS 2.0'''))
         metas = ['dtb:uid', 'dtb:depth', 'dtb:totalPageCount',
                  'dtb:maxPageNumber', 'dtb:generator']
         for meta in metas:
-            meta_tag = self.doc.createElement('meta')
+            meta_tag = self.document.createElement('meta')
             meta_tag.setAttribute('name', meta)
             self.head.appendChild(meta_tag)
 
+    def reset_state(self):
+        """
+        Resets the internal state variables to defaults, also used in __init__
+        to set them at the beginning.
+        """
+        self.article = None
+        self.all_articles = []
+        self.doi = ''
+        self.all_dois = []
+        self.article_doi = ''
+        self.journal_doi = ''
+        self.play_order = 1
+        
+        #Reset the other metadata and other structures
+        self.reset_metadata()
+        self.reset_lists()
+
+    def reset_metadata():
+        """
+        THe NCX file does not truly exist for metadata, but it has a few
+        elements held over from the Daisy Talking Book specification. The 
+        """
+
+    def reset_lists(self):
+        """
+        Resets the internal states of the lists of items: List of Figures, List
+        of Tables, and List of Equations. This is distinct from the navMap.
+        """
+        self.list_of_figures = []
+        self.list_of_tables = []
+        self.list_of_equations = []
+
+    def use_collection_mode(self):
+        """Enables Collection Mode, sets self.collection_mode to True"""
+        self.collection_mode = True
+
+
+    def use_single_mode(self):
+        """Disables Collection Mode, sets self.collection_mode to False"""
+        self.collection_mode = False
 
