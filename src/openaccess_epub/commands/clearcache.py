99c072b8833b9ab6c3a12f8ca035effcd0a29f62
Paul Barton <pablo.barton@gmail.com>
2014-02-24 13:45:21 -0500

    Adding in a working clearcache command script

--- src/openaccess_epub/commands/clearcache.py
+++ src/openaccess_epub/commands/clearcache.py
@@ -19,19 +19,42 @@ Clearcache Specific Options:
                    the command (because you are cautious and wise)
 
 Recognized commands for oaepub clearcache are:
-  all     Delete all cached data: images, logs, and XML
+  all     Delete all cached data: images, logs
   images  Delete only the cached image files
   logs    Delete only the cached log files
   manual  Print out the cache location then exit
-  xml     Delete only the cached XML files
 
 Remember that you can disable any or all caching. Caching is very helpful for
-development, and may not be necessary for all users. If you want to manually
+development, but may not be necessary for all users. If you want to manually
 alter your cache, you can use 'oaepub clearcache manual' to tell you where the
 cache is located.
 """
 
+#Standard Library modules
+import os
+import platform
+import shutil
+import subprocess
+import sys
+
+#Non-Standard Library modules
 from docopt import docopt
+
+#OpenAccess_EPUB modules
+import openaccess_epub.utils
+
+config = openaccess_epub.utils.load_config_module()
+
+
+def empty_it(path, dry_run):
+    if dry_run:
+        print('Deleting all contents of {0}'.format(path))
+        return
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            os.remove(os.path.join(root, f))
+        for d in dirs:
+            shutil.rmtree(os.path.join(root, d))
 
 
 def main(argv=None):
@@ -39,7 +62,34 @@ def main(argv=None):
                   argv=argv,
                   version='OpenAccess_EPUB Docoptify 0.1',
                   options_first=True)
-    print(args)
+
+    cache_loc = openaccess_epub.utils.cache_location()
+
+    if args['COMMAND'] == 'manual':
+        # We'll *try* to launch a file browser, at least print cache location
+        plat = platform.platform()
+        if plat.startswith('Windows'):
+            os.startfile(cache_loc)
+        elif plat.startswith('Darwin'):
+            subprocess.Popen(['open', cache_loc])
+        elif plat.startswith('Linux'):
+            try:
+                subprocess.Popen(['xdg-open', cache_loc])
+            except OSError:
+                pass
+        sys.exit('The cache is located at {0}'.format(cache_loc))
+
+    elif args['COMMAND'] == 'logs':
+        empty_it(os.path.join(cache_loc, 'logs'), dry_run=args['--dry-run'])
+        sys.exit()
+    elif args['COMMAND'] == 'images':
+        empty_it(config.image_cache, dry_run=args['--dry-run'])
+        sys.exit()
+    elif args['COMMAND'] == 'all':
+        empty_it(os.path.join(cache_loc, 'logs'), dry_run=args['--dry-run'])
+        empty_it(config.image_cache, dry_run=args['--dry-run'])
+        sys.exit()
+
 
 if __name__ == '__main__':
     main()
