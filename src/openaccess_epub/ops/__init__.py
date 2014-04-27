cb79b3b707f760323bf2bb4a57593b2100ea91b5
SavinaRoja <pablo.barton@gmail.com>
2014-04-26 22:19:15 -0400

    removing old ops subpackage

--- src/openaccess_epub/ops/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openaccess_epub.ops.opsmeta import OPSMeta
-from openaccess_epub.ops.opsplos import OPSPLoS
-from openaccess_epub.ops.opsfrontiers import OPSFrontiers
-
-import logging
-
-log = logging.getLogger('openaccess_epub.ops')
